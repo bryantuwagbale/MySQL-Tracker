a041b895aadcb798a621a4cfc4e7613f943d3a6f
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-07-15 18:22:52 -0500

    tiny update

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,7 +11,7 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Web Developer', 90000, 1),
+('Web Developer', 100000000000000, 1),
 ('Accountant', 70000, 2),
 ('Paralegal', 50000, 3),
 ('Manager', 70000, 4),
@@ -20,9 +20,9 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('John', 'Smith', 1, 458),
-('Ronald', 'Young', 2, 276),
-('David', 'Miller', 3, 486),
-('Maria', 'Hall', 4, 126),
-('Linda', 'Martin', 5, 724),
-('Taylor', 'Wilson', 6, 157);
+('John', 'Doe', 1, 462),
+('Jane', 'Doe', 2, 216),
+('Maxwell', 'Miller', 3, 606),
+('Maria', 'Mortimer', 4, 490),
+('Linda', 'Larkin', 5, 198),
+('Teyona', 'Taylor', 6, 167);
